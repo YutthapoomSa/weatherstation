c64b6b6ab8394b0441206bcb75470a722f15fa34
cryingFreeman <joth14@gmail.com>
2023-02-02 16:22:20 +0700

    123

--- src/api/device/device.controller.ts
+++ src/api/device/device.controller.ts
@@ -3,8 +3,9 @@ import { DeviceService } from './service/device.service';
 import { CreateReqDeviceDTO, CreateResDeviceDTO } from './dto/create-device.dto';
 import { UpdateDeviceDto } from './dto/update-device.dto';
 import { ApiDeviceService } from './service/api-device.service';
-import { ApiOperation, ApiOkResponse } from '@nestjs/swagger';
+import { ApiOperation, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('device')
 @Controller('device')
 export class DeviceController {
     constructor(private readonly apiDeviceService: ApiDeviceService) {}
